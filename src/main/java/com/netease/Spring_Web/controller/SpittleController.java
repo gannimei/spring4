9af5883ef129bc56571c38b8d21050d6d662caca
gannimei <tczhoulan@163.com>
2018-01-01 20:58:32 +0800

    error solve

--- src/main/java/com/netease/Spring_Web/controller/SpittleController.java
+++ src/main/java/com/netease/Spring_Web/controller/SpittleController.java
@@ -4,19 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.netease.Spring_Web.dao.SpittleRepository;
 
 @Controller
-@RequestMapping("/spittle")
+@RequestMapping("/spittles")
 public class SpittleController {
 	
-	/*@Autowired
+	@Autowired
 	private SpittleRepository spittleRepository; 
 	
+	@RequestMapping(method = RequestMethod.GET)
 	public String spittles(Model model) {
 		model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
 		return "spittles";
-	}*/
+	}
 	
 }
